08eb2a02d4342bbc4aae0589a8761a5c921411c4
Yuki Shinji <92155634+YukiShinji@users.noreply.github.com>
2024-03-28 15:12:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+import sys
+
+
+def add_new_user(name: str, phone: str, filename: str):
+    """
+    Добавление нового пользователя.
+    """
+    new_line = '\n' if read_all(filename) != "" else ''
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(f"{new_line}{name} - {phone}")
+
+
+def read_all(filename: str) -> str:
+    """
+    Возвращает все содержимое телефонной книги.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def search_user(filename: str, data: str) -> str:
+    """
+    Поиск записи по критерию data.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        list_1 = file.read().split("\n")
+    result = []
+    result = [i for i in list_1 if data in i]
+    if not result:
+        return "По указанному значению совпадений не найдено"
+    return "\n".join(result)
+
+
+def transfer_data(source: str, dest: str, num_row: int):
+    with open(source) as first, open(dest, 'w') as second:
+        lines = first.readlines()
+        data = lines[int(num_row) - 1]
+        second.write(data)    
+    pass
+
+
+INFO_STRING = """
+Выберите ркжим работы:
+1 - вывести все данные
+2 - добавление нового пользователя
+3 - поиск
+4 - перенос записи в другой файл
+"""
+
+file = "Text1.txt"
+
+if file not in os.listdir():
+    print("указанное имя файла отсутсвует")
+    sys.exit()
+
+
+while True:
+    mode = int(input(INFO_STRING))
+    if mode == 1:
+        print(read_all(file))
+    elif mode == 2:
+        name = input("Введите Ваше имя: ")
+        phone = input("Введите Ваш телефон: ")
+        add_new_user(name, phone, file)
+    elif mode == 3:
+        data = input("Введите значение: ")
+        print(search_user(file, data))
+    elif mode == 4:
+        # Тут нужно вызвать функцию с аргументами
+        first = input("Введите имя исходного файла: ")
+        second = input("Введите имя файла куда переносим данные: ")
+        num_str = input("Введите номер переносимой строки: ")
+        transfer_data(first, second, num_str)
+        pass
